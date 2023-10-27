733351e5fa0437c4105b406d40df7512055daabe
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-26 21:26:45 -0300

    ✨ feat: setup global style

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -2,6 +2,9 @@ import { Outlet } from "react-router-dom"
 
 export default function Root() {
   return (
+      <main className="container min-h-screen">
+        <Outlet />
+      </main>
     <main className="min-h-screen bg-zinc-50">
       <Outlet />
     </main>
