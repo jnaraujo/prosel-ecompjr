196010147d66556e2ad40943f7847009dc105b87
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-26 21:21:39 -0300

    ✨ feat: add fonts

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "8px",
+      screens: {
+        "2xl": "1200px",
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
