ce04784def4df87bf7658591524407315849a86d
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-27 11:31:55 -0300

    ✨ feat: add react-hot-toast

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,15 +1,26 @@
 import { Outlet } from "react-router-dom"
+import { Toaster } from "react-hot-toast"
 import Navbar from "../components/navbar"
 import ScrollToHashElement from "../components/scroll-to-hash-element"
 
 export default function Root() {
   return (
     <>
-      <ScrollToHashElement />
       <Navbar />
       <main className="flex flex-1 flex-col">
         <Outlet />
       </main>
+
+      <ScrollToHashElement />
+      <Toaster
+        toastOptions={{
+          duration: 5000,
+          style: {
+            fontFamily: "var(--font-inter)",
+          },
+        }}
+        position="top-center"
+      />
     </>
   )
 }
