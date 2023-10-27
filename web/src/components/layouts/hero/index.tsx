5225bb428eee4dbb7000cdf73e8ac2c17593975f
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-27 11:54:31 -0300

    ✨ feat: improved hero

--- web/src/components/layouts/hero/index.tsx
+++ web/src/components/layouts/hero/index.tsx
@@ -6,7 +6,7 @@ import Button from "../../button"
 
 export default function Hero() {
   return (
-    <section className="relative flex min-h-[95vh] flex-1 flex-col items-center justify-center md:gap-16">
+    <section className="relative flex min-h-[90vh] flex-1 flex-col items-center justify-center md:min-h-[95vh] md:gap-16">
       <div className="flex min-h-[400px] max-w-[530px] flex-col items-center justify-center gap-4 px-4 text-center">
         <h1 className="text-3xl font-bold text-zinc-800 md:text-5xl">
           Conquiste o <span className="text-brand-blue">Mundo Digital</span> com
@@ -32,7 +32,7 @@ export default function Hero() {
         {images.map((item) => (
           <div
             key={item}
-            className="mx-4 h-48 overflow-hidden rounded-md shadow-md transition duration-300 ease-in-out hover:shadow-xl"
+            className="mx-4 h-36 overflow-hidden rounded-md shadow-md transition duration-300 ease-in-out hover:shadow-xl md:h-48"
           >
             <img src={item} alt="logo" className="aspect-video h-full" />
           </div>
