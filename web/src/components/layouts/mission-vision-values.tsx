c04db3617c425aca89d041ecbc15a56f8342de96
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-26 22:28:19 -0300

    ✨ feat: add MissionVisionValues

---
+++ web/src/components/layouts/mission-vision-values.tsx
@@ -0,0 +1,36 @@
+export default function MissionVisionValues() {
+  return (
+    <section className="bg-brand-blue py-24">
+      <div className="container grid grid-cols-1 gap-8 md:grid-cols-3 md:gap-16">
+        <div className="space-y-2 text-center">
+          <h3 className="text-2xl font-semibold text-zinc-50">Missão</h3>
+          <p className="text-zinc-200">
+            Através da vivência empresarial e trabalho em equipe, formar
+            profissionais inovadores, comprometidos e capazes de vencer desafios
+            futuros.
+          </p>
+        </div>
+
+        <div className="space-y-2 text-center">
+          <h3 className="text-2xl font-semibold text-zinc-50">Visão</h3>
+          <p className="text-zinc-200">
+            Ser uma empresa que realiza projetos de alto impacto mais conectada
+            ao movimento empresa júnior e com grande reconhecimento dentro e
+            fora da universidade .
+          </p>
+        </div>
+
+        <div className="space-y-2 text-center">
+          <h3 className="text-2xl font-semibold text-zinc-50">Valores</h3>
+          <p className="text-zinc-200">
+            Os valores de uma empresa guiam a conduta da mesma, portanto a
+            Ecompjr baseia-se nos seguintes valores. Protagonismo,
+            Cooperatividade Assiduidade, Resiliência, Constância em resultados,
+            Evolução com os erros, Conexão com o movimento, Foco na qualidade,
+            Ética e Empatia.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
--- web/src/routes/home.tsx
+++ web/src/routes/home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/layouts/hero"
+import MissionVisionValues from "../components/layouts/mission-vision-values"
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <MissionVisionValues />
     </>
   )
 }
