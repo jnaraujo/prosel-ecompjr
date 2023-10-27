ec5f2f3808cff14e4007ff034eba93efafb0b160
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-26 21:25:20 -0300

    🚨 lint

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import webfontDownload from 'vite-plugin-webfont-dl';
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import webfontDownload from "vite-plugin-webfont-dl"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
