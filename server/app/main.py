1fd9256832d75fa662d68c2bda3a13efa01ea3c7
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-30 17:43:29 -0300

    ✨ feat: allow all cors

--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from database import engine, Base
 from routers import forms_routes, user_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
