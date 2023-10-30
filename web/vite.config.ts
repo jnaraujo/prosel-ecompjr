318f8ab285706bdf5f262f6e10728f57b598ae86
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-30 20:51:57 -0300

    🔥 chore: config path mapping

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from "vite"
 import webfontDownload from "vite-plugin-webfont-dl"
 import react from "@vitejs/plugin-react"
@@ -5,4 +6,9 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), webfontDownload()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
