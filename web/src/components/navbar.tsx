3f1eabaf7941d5c6a084eb84a28807129e73adf8
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-30 18:19:05 -0300

    ✨ feat: improved navbarf

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom"
 import logo from "../assets/logo.webp"
 
 export default function Navbar() {
   return (
     <nav className="container flex items-center justify-start py-2">
-      <img
-        src={logo}
-        alt="logo"
-        width={128}
-        height={44}
-        className="h-9 object-contain md:h-12"
-      />
+      <Link to="/">
+        <img
+          src={logo}
+          alt="logo"
+          width={128}
+          height={44}
+          className="h-9 object-contain md:h-10"
+        />
+      </Link>
     </nav>
   )
 }
