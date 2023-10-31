7967ede4d9c5c5d649e8b6ab9362754ae685cfe7
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-31 12:42:49 -0300

    ✨ feat: add env

--- server/app/database.py
+++ server/app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from envs import database_url
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/ecompjr"
+SQLALCHEMY_DATABASE_URL = database_url
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
