677ffab867b8a33335212ccee27d79757ca6334e
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-31 21:29:26 -0300

    feat: add login button to navbar

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
 import logo from "../assets/logo.webp"
+import Button from "./button"
 
 export default function Navbar() {
   return (
-    <nav className="container flex items-center justify-start py-2">
+    <nav className="container flex items-center justify-between py-2">
       <Link to="/">
         <img
           src={logo}
@@ -13,6 +14,10 @@ export default function Navbar() {
           className="h-9 object-contain md:h-10"
         />
       </Link>
+
+      <Button asChild className="w-fit py-1" variant="outline">
+        <Link to="/login">Login</Link>
+      </Button>
     </nav>
   )
 }
