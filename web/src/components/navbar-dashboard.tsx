faeb0cfa0350ccef46a5ccc34daf4add2d946ef5
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-31 21:38:39 -0300

    ✨ feat: add aria label

--- web/src/components/navbar-dashboard.tsx
+++ web/src/components/navbar-dashboard.tsx
@@ -42,7 +42,10 @@ export default function NavbarDashboard() {
         </ul>
 
         <DropdownMenu>
-          <DropdownMenuTrigger className="flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-700 text-zinc-700">
+          <DropdownMenuTrigger
+            className="flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-700 text-zinc-700"
+            aria-label="Menu de usuário"
+          >
             <User size={24} />
           </DropdownMenuTrigger>
           <DropdownMenuContent>
