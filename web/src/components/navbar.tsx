37475fd34efbaba6abe5ba5842a63e1be3485b1f
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-11-01 23:16:48 -0300

    ✨ feat: remove navbar bg on login page

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,27 +1,36 @@
 import { Link, useLocation } from "react-router-dom"
 import logo from "../assets/logo.webp"
 import Button from "./button"
+import { cn } from "@/lib/utils"
 
 export default function Navbar() {
   const location = useLocation()
 
-  return (
-    <nav className="container flex items-center justify-between py-2">
-      <Link to="/">
-        <img
-          src={logo}
-          alt="logo"
-          width={128}
-          height={40}
-          className="h-9 object-contain md:h-10"
-        />
-      </Link>
+  const isOnLoginpage = location.pathname === "/login"
 
-      {location.pathname !== "/login" && (
-        <Button asChild className="w-fit py-1" variant="outline">
-          <Link to="/login">Login</Link>
-        </Button>
-      )}
-    </nav>
+  return (
+    <header
+      className={cn("py-2", {
+        "bg-blue-50": !isOnLoginpage,
+      })}
+    >
+      <nav className="container flex items-center justify-between">
+        <Link to="/">
+          <img
+            src={logo}
+            alt="logo"
+            width={128}
+            height={40}
+            className="h-9 object-contain md:h-10"
+          />
+        </Link>
+
+        {!isOnLoginpage && (
+          <Button asChild className="w-fit py-1" variant="outline">
+            <Link to="/login">Login</Link>
+          </Button>
+        )}
+      </nav>
+    </header>
   )
 }
