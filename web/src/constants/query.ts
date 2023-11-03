179debb1ab083c476818a5580cabcd08e2b8d450
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-11-03 12:02:26 -0300

    ✨ feat: update stale time for forms query

--- web/src/constants/query.ts
+++ web/src/constants/query.ts
@@ -1,2 +1,2 @@
 export const USERS_QUERY_STALE_TIME_IN_MS = 1000 * 60 // 1 minuto
-export const FORMS_QUERY_STALE_TIME_IN_MS = 1000 * 5 // 5 segundos
+export const FORMS_QUERY_STALE_TIME_IN_MS = 1000 * 10 // 5 segundos
